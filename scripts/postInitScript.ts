1779efaba07a84051e4deff8d5809185eba9e945
QasimJaffry <qhassan1213@gmail.com>
2023-03-02 14:30:49 +0500

    added semantic release and husky

--- scripts/postInitScript.ts
+++ scripts/postInitScript.ts
@@ -7,16 +7,33 @@ import { inquire } from './generator';
 
 const spinner = ora('Optional libraries setup');
 
-new Promise((resolve) => {
-  spinner.start();
-  inquire(resolve);
-})
-  .then(() => {
-    spinner.succeed();
-  })
-  .catch((error) => {
-    spinner.fail(error);
-    throw new Error(
-      'Something went wrong during the post init script execution',
-    );
-  });
+// new Promise((resolve) => {
+//   spinner.start();
+//   inquire(resolve);
+// })
+//   .then(() => {
+//     spinner.succeed();
+//   })
+//   .catch((error) => {
+//     spinner.fail(error);
+//     throw new Error(
+//       'Something went wrong during the post init script execution',
+//     );
+//   });
+
+
+
+ new Promise((resolve) => {
+      spinner.start();
+      console.log("\nTemplate initialization successful! 🚀");
+    //   resolve();
+    })
+      .then(() => {
+        spinner.succeed();
+      })
+      .catch(() => {
+        spinner.fail();
+        throw new Error(
+          "Something went wrong during the post init script execution"
+        );
+      });
