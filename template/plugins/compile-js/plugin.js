e3e22d9f66677c9afc3f35c9412913186c168df8
QasimJaffry <qhassan1213@gmail.com>
2023-03-06 16:15:28 +0500

    🐛: added template

---
+++ template/plugins/compile-js/plugin.js
@@ -0,0 +1,12 @@
+const {execSync} = require('child_process');
+
+module.exports = {
+  async apply(value, previousValues) {
+    return new Promise(async resolve => {
+      console.log('\n');
+      console.log('📦 Installing TypeScript');
+
+      resolve();
+    });
+  },
+};
---
+++ template/plugins/printSuccess/plugin.js
@@ -0,0 +1,15 @@
+const {green, blue, yellow, red} = require('kleur');
+
+module.exports = {
+  async apply(value, previousValues) {
+    return new Promise(resolve => {
+      console.log('\n');
+
+      console.log(blue('  TYPESCRIPT '));
+
+      console.log('\n');
+
+      resolve();
+    });
+  },
+};
