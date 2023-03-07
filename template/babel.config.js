29e2a251cb5109678b755f0fe40fe11705501bc5
QasimJaffry <qhassan1213@gmail.com>
2023-03-07 14:52:35 +0500

    ✨: translation and alias

--- template/babel.config.js
+++ template/babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
         root: ['./src'],
         extensions: ['.js', '.json'],
         alias: {
-          '@': './src',
+          '@': ['src/*'],
         },
       },
     ],
